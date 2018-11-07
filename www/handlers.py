9bde6adbcd44530433602563106288fbcf876a90
mht <hitomeng@outlook.com>
2018-11-07 23:52:02 +0800

    add register.html

--- www/handlers.py
+++ www/handlers.py
@@ -12,7 +12,7 @@ from www.model.user import User, Comment, Blog, next_id
 
 @get('/')
 async def index(request):
-    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    summary = 'This a simple test!'
     blogs = [
         Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
         Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
@@ -21,4 +21,19 @@ async def index(request):
     return {
         '__template__': 'blogs.html',
         'blogs': blogs
+    }
+
+
+@get('/api/users')
+def api_get_users(*, page='1'):
+    users = yield from User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
+
+
+@get('/register')
+def register():
+    return {
+        '__template__': 'register.html'
     }
